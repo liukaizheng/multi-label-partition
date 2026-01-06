c1c6a185196b82cf3a62ae39bea4d966bdb331f3
Kaizheng Liu <lkzheg@gmail.com>
2026-01-06 14:31:54 +0800

    Enhance mesh handling by adding msh file support and updating launch configuration

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@
 #include <array>
 #include <boost/functional/hash.hpp>
 
+#include <mshio/mshio.h>
+
 #include <CLI/CLI.hpp>
 #include <gpf/handles.hpp>
 #include <gpf/ids.hpp>
@@ -85,6 +87,7 @@ void write_msh(const std::string& name, const VMat& V, const TMat& T) {
         std::println(out, "f {} {} {}", t(1) + 1, t(3) + 1, t(2) + 1);
     }
 }
+
 auto read_colored_mesh(const std::string& file_name) {
     std::ifstream input_file(file_name, std::ios::in);
     int n_v, n_f, n_e;
@@ -148,6 +151,42 @@ auto read_colored_mesh(const std::string& file_name) {
     );
 }
 
+auto read_msh(const std::string& file_name) {
+    mshio::MshSpec spec = mshio::load_msh(file_name);
+    // Build a map from node tag to index
+    std::unordered_map<std::size_t, std::size_t> tag_to_index;
+    std::vector<std::array<double, 3>> points;
+    for (const auto& block : spec.nodes.entity_blocks) {
+        for (std::size_t i = 0; i < block.num_nodes_in_block; ++i) {
+            std::size_t tag = block.tags[i];
+            tag_to_index[tag] = points.size();
+            points.push_back({
+                block.data[i * 3 + 0],
+                block.data[i * 3 + 1],
+                block.data[i * 3 + 2]
+            });
+        }
+    }
+    std::vector<std::array<std::size_t, 4>> tets;
+    for (const auto& block : spec.elements.entity_blocks) {
+        // Element type 4 is tetrahedron in Gmsh
+        if (block.element_type == 4) {
+            std::size_t nodes_per_elem = mshio::nodes_per_element(block.element_type);
+            for (std::size_t i = 0; i < block.num_elements_in_block; ++i) {
+                // data layout: [elem_tag, node1, node2, node3, node4, ...]
+                std::size_t offset = i * (nodes_per_elem + 1);
+                tets.push_back({
+                    tag_to_index[block.data[offset + 1]],
+                    tag_to_index[block.data[offset + 2]],
+                    tag_to_index[block.data[offset + 3]],
+                    tag_to_index[block.data[offset + 4]]
+                });
+            }
+        }
+    }
+    return std::make_pair(std::move(points), std::move(tets));
+}
+
 auto build_triangle_groups(
     const std::vector<std::array<double, 3>>& points,
     const std::vector<std::vector<std::size_t>>& faces,
@@ -168,6 +207,69 @@ auto build_triangle_groups(
         triangle_groups.emplace_back(std::move(triangles));
     }
     return triangle_groups;
+}
+
+auto build_tet_mesh1(
+    const std::vector<std::array<double, 3>>& points,
+    const std::vector<std::array<std::size_t, 4>>& tet_indices
+) {
+    auto hash = [](const std::array<std::size_t, 3>& key) {
+        return boost::hash_value(key);
+    };
+    auto hash_key = [](const std::array<std::size_t, 3>& key) {
+        auto ret = key;
+        std::sort(ret.begin(), ret.end());
+        return ret;
+    };
+    std::unordered_map<std::array<std::size_t, 3>, std::size_t, decltype(hash)> face_index_map(tet_indices.size() * 2, std::move(hash));
+    std::vector<std::array<std::size_t, 3>> faces;
+    std::vector<std::array<std::size_t, 2>> face_tets;
+    face_tets.reserve(tet_indices.size() * 2);
+    std::vector<tet_mesh::Tet> tets;
+    for (std::size_t i = 0; i < tet_indices.size(); ++i) {
+        const auto& t = tet_indices[i];
+        std::array<std::size_t, 4> tet_faces;
+        std::size_t idx = 0;
+        for (auto&& face : {
+            std::array<std::size_t, 3>{{t[2], t[1], t[3]}},
+            std::array<std::size_t, 3>{{t[0], t[2], t[3]}},
+            std::array<std::size_t, 3>{{t[1], t[0], t[3]}},
+            std::array<std::size_t, 3>{{t[0], t[1], t[2]}}
+        }) {
+            auto iter = face_index_map.emplace(hash_key(face), faces.size());
+            if (iter.second) {
+                faces.emplace_back(std::move(face));
+                face_tets.emplace_back(std::array<std::size_t, 2>{{i, gpf::kInvalidIndex}});
+                tet_faces[idx] = gpf::oriented_index(iter.first->second, false);
+            } else {
+                face_tets[iter.first->second][1] = i;
+                tet_faces[idx] = gpf::oriented_index(iter.first->second, true);
+            }
+            idx += 1;
+        }
+        tets.emplace_back(tet_mesh::Tet{.vertices{gpf::VertexId{t[0]}, gpf::VertexId{t[1]}, gpf::VertexId{t[2]}, gpf::VertexId{t[3]}}, .faces{std::move(tet_faces)}});
+    }
+    auto mesh = tet_mesh::TetMesh::new_in(std::move(faces));
+    for (auto& tet : tets) {
+        const auto& vertices = tet.vertices;
+        auto& edges = tet.edges;
+        std:size_t idx = 0;
+        for (std::size_t i = 0; i < 3; i++) {
+            for (std::size_t j = i + 1; j < 4; j++) {
+                const auto eid = mesh.e_from_vertices(vertices[i], vertices[j]);
+                edges[idx++] = eid;
+            }
+        }
+    }
+    for (auto v : mesh.vertices()) {
+        v.data().property.pt = points[v.id.idx];
+    }
+
+    for (auto f : mesh.faces()) {
+        f.data().property.cells = std::move(face_tets[f.id.idx]);
+    }
+
+    return std::make_pair(std::move(mesh), std::move(tets));
 }
 
 auto build_tet_mesh(
@@ -263,7 +365,7 @@ auto setup_neg_distance(tet_mesh::TetMesh& mesh, const std::vector<std::vector<T
         for (auto [d, tree] : ranges::zip_view(dists, trees)) {
             // auto [closest_point, primitive] = tree.closest_point_and_primitive(pt);
             // auto fid = primitive - triangle_groups[idx++].begin();
-            d = -tree.squared_distance(pt);
+            d = -std::sqrt(tree.squared_distance(pt));
         }
     }
 }
@@ -271,32 +373,36 @@ auto setup_neg_distance(tet_mesh::TetMesh& mesh, const std::vector<std::vector<T
 int main(int argc, char* argv[]) {
     CLI::App app { "multi-label-partition" };
     std::string mesh_path;
+    std::string msh_path;
     app.add_option("-m,--mesh", mesh_path, "Path to mesh file")->required();
+    app.add_option("-t,--msh", msh_path, "Path to msh file")->required();
     CLI11_PARSE(app, argc, argv);
 
     std::vector<std::array<std::size_t, 3>> colors;
     auto [points, faces, label_face_groups] = read_colored_mesh(mesh_path);
     auto triangle_groups = build_triangle_groups(points, faces, label_face_groups);
+    auto[tet_points, tet_indices] = read_msh(msh_path);
 
-    VMat V(points.size(), 3);
-    FMat F(faces.size(), 3);
-    for (std::size_t i = 0; i < points.size(); ++i) {
-        const auto& p = points[i];
-        V.row(i) << p[0], p[1], p[2];
-    }
-    for (std::size_t i = 0; i < faces.size(); ++i) {
-        const auto& f = faces[i];
-        F.row(i) << f[0], f[1], f[2];
-    }
+    // VMat V(points.size(), 3);
+    // FMat F(faces.size(), 3);
+    // for (std::size_t i = 0; i < points.size(); ++i) {
+        // const auto& p = points[i];
+        // V.row(i) << p[0], p[1], p[2];
+    // }
+    // for (std::size_t i = 0; i < faces.size(); ++i) {
+        // const auto& f = faces[i];
+        // F.row(i) << f[0], f[1], f[2];
+    // }
 
-    VMat TV;
-    TMat TT;
-    FMat TF;
+    // VMat TV;
+    // TMat TT;
+    // FMat TF;
 
-    igl::copyleft::tetgen::tetrahedralize(V, F, "pa0.004V", TV, TT, TF);
-    auto [tet_mesh, tets] = build_tet_mesh(TV, TT, TF);
+    // igl::copyleft::tetgen::tetrahedralize(V, F, "pa0.004V", TV, TT, TF);
+    // auto [tet_mesh, tets] = build_tet_mesh(TV, TT, TF);
+
+    auto [tet_mesh, tets] = build_tet_mesh1(tet_points, tet_indices);
     setup_neg_distance(tet_mesh, triangle_groups);
     do_material_interface(tets, tet_mesh);
-    write_msh("123.obj", TV, TT);
     return 0;
 }
